b0cd62945cf334c600396e12031e86c0b5fd7f38
Obi <pc@Obie.local>
2025-12-16 09:57:39 +0800

    Analyticsreact

--- src/main.tsx
+++ src/main.tsx
@@ -2,8 +2,7 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
 import "./index.css"; // <-- important
-import { Analytics } from "@vercel/analytics/next"
-
+import { Analytics } from "@vercel/analytics/react"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
